61db46b570af6c8887cf3d565be48c8ec2d5da88
Coral Winter <wj_0575@sjtu.edu.cn>
2025-06-18 14:22:45 +0800

    。

--- main.py
+++ main.py
@@ -337,9 +337,6 @@ def get_train_info(x, date=auto_date):
             train_list[x] = data
         finally:
             lock_train_list.release()
-            return None
-    return None
-
 
 try_times = 5
 def get_all_target_info(key, mode):
